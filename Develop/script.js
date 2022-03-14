3df17315e7f2b082fa3be4dbd2b6d3b67f8c5ffe
Norse <cody.samborski@gmail.com>
2022-03-14 18:31:37 -0500

    finished password generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,66 +1,92 @@
 // array of upperCase letters
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var upperCases = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 // array of lowerCase letters
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowerCases = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // array of specialChars "~`!@#$%^&*()_-+={[}]|:;'<,>.?/"
 var specialChars = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", "|", ":", ";", "'", "<", ",", ">", ".", "?", "/"];
 // array of numbers
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-var randomNumber = function(min, max) {
-  var value = Math.floor(Math.random() * (max - min +1) + min);
-
-  return value;
-};
-
-var generatePassword = function(){
+// set passLength to 0
+var passLength = 0;
+// main function
+var generatePassword = function() {
+  // welcome alert
   window.alert("Welcome to the password generator!");
-  var passLength = window.prompt("Please choose the amount of characters you would like in your password. Enter a numerical value between 8-128.");
-    if (passLength < 8 || passLength > 128) {
-      window.alert("You need to provide a valid answer. Please try again!");
-      return generatePassword();
+  //function to determine password length
+  var getPassLength = function() {
+    passLength = window.prompt("Please choose the amount of characters you would like in your password. Enter a numerical value between 8-128.");
+    while (passLength === "" || passLength === null || passLength < 8 || passLength > 128) {
+      passLength = alert("You need to provide a valid answer. Please try again!")
+      return getPassLength();
     }
+    return passLength;
+  };
+  
+// call function/log in console.log
+getPassLength();
+console.log(passLength);
 
-    var initialArray = [];
+//set masterArray and randomArray to empty
+var masterArray = [];
+var randomArray = [];
 
-    var clc = window.confirm("Would you like to include lowercase letters in this password? Reply with 'OK' for yes and 'CANCEL' for no."); 
-    console.log(clc);
-    if (clc) {
-      initialArray.splice(0, 0, lowerCase)
-    }
-    var cuc = window.confirm("Would you like to include uppercase letters in this password? Reply with 'OK' for yes and 'CANCEL' for no.");
-    console.log(cuc);
+// function to confirm and splice what characters types user would like for password into masterArray.
+var confirmations = function() {
+  var cuc = window.confirm("Would you like to include uppercase letters 'A-Z' in this password? Reply with 'OK' for yes and 'CANCEL' for no.");
+  console.log(cuc);
     if (cuc) {
-      initialArray.splice(0, 0, upperCase)
+      masterArray.splice(0, 0, upperCases)
     }
-    var csc = window.confirm("Would you like to include the following special charactars in this password: '~`!@?#$%^&*()_-+={[}]|:;'<,>./' ? Reply with 'OK' for yes and 'CANCEL' for no.")
-    console.log(csc);
+  var clc = window.confirm("Would you like to include lowercase letters 'a-z' in this password? Reply with 'OK' for yes and 'CANCEL' for no."); 
+  console.log(clc);
+    if (clc) {
+       masterArray.splice(0, 0, lowerCases)
+    }
+  var csc = window.confirm("Would you like to include the following special charactars '~`!@?#$%^&*()_-+={[}]|:;'<,>./' in this password? Reply with 'OK' for yes and 'CANCEL' for no.")
+  console.log(csc);
     if (csc) {
-      initialArray.splice(0, 0, specialChars)
+      masterArray.splice(0, 0, specialChars)
     }
-    var cn = window.confirm("Would you like to include numbers in this password? Reply with 'OK' for yes and 'CANCEL' for no.");
-    console.log(cn);
+  var cn = window.confirm("Would you like to include numbers '0-9' in this password? Reply with 'OK' for yes and 'CANCEL' for no.");
+  console.log(cn);
     if (cn) {
-      initialArray.splice(0, 0, numbers)
-    }
-
-    console.log(passLength);  
-    console.log(initialArray);
+      masterArray.splice(0, 0, numbers)
+    }   
+  console.log(masterArray);
+  //validate that at least one option was selected
+  if ((!cn) && (!clc) && (!csc) && (!cuc)) {
+    window.alert("You must select at least one option");
+    return confirmations();
+  } else {
+    return masterArray;
+  }
 };
 
+//call confirmations()
+confirmations();
 
+// generate random char from arrays, build password in string.
+var i = 0;
+var finalPassword = "";
+while (i < passLength) {
+  var randomArray = masterArray[Math.floor(Math.random()*masterArray.length)];
+  var randomChar = randomArray[Math.floor(Math.random()*randomArray.length)];
+  console.log(randomChar);
+  i = i + 1;
+  finalPassword = finalPassword + randomChar;
+};
+return finalPassword;
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 
