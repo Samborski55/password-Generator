192b479757863e99935073cab6f671269031b998
Norse <cody.samborski@gmail.com>
2022-03-13 23:23:23 -0500

    built arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,18 @@
-// Assignment code here
+// array of upperCase letters
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// array of lowerCase letters
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// array of specialChars "~`!@#$%^&*()_-+={[}]|:;'<,>.?/"
+var specialChars = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", "|", ":", ";", "'", "<", ",", ">", ".", "?", "/"];
+// array of numbers
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min +1) + min);
+
+  return value;
+};
+
 var generatePassword = function(){
   window.alert("Welcome to the password generator!");
   var passLength = window.prompt("Please choose the amount of characters you would like in your password. Enter a numerical value between 8-128.");
@@ -6,20 +20,34 @@ var generatePassword = function(){
       window.alert("You need to provide a valid answer. Please try again!");
       return generatePassword();
     }
+
+    var initialArray = [];
+
+    var clc = window.confirm("Would you like to include lowercase letters in this password? Reply with 'OK' for yes and 'CANCEL' for no."); 
+    console.log(clc);
+    if (clc) {
+      initialArray.splice(0, 0, lowerCase)
+    }
+    var cuc = window.confirm("Would you like to include uppercase letters in this password? Reply with 'OK' for yes and 'CANCEL' for no.");
+    console.log(cuc);
+    if (cuc) {
+      initialArray.splice(0, 0, upperCase)
+    }
+    var csc = window.confirm("Would you like to include the following special charactars in this password: '~`!@?#$%^&*()_-+={[}]|:;'<,>./' ? Reply with 'OK' for yes and 'CANCEL' for no.")
+    console.log(csc);
+    if (csc) {
+      initialArray.splice(0, 0, specialChars)
+    }
+    var cn = window.confirm("Would you like to include numbers in this password? Reply with 'OK' for yes and 'CANCEL' for no.");
+    console.log(cn);
+    if (cn) {
+      initialArray.splice(0, 0, numbers)
+    }
+
     console.log(passLength);  
-  // array of upperCase
-  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  // array of lowerCase
-  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  // array of specialChars ]
-  var specialChars = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", "|", ":", ";", "'", "<", ",", ">", ".", "?", "/"];
-  // array of numbers
-  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    console.log(upperCase);
-    console.log(lowerCase);
-    console.log(specialChars);
-    console.log(numbers);
-}
+    console.log(initialArray);
+};
+
 
 
 // Get references to the #generate element
@@ -34,6 +62,8 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
